eb09dc8a665dd8a81cb0a983d13cc73cd134ca6f
jeffersoncardoso <jefferson.mcardoso@yahoo.com.br>
2015-10-25 13:56:21 -0200

    adicionado projeto

---
+++ src/isp/ScrumMaster.php
@@ -0,0 +1,19 @@
+<?php 
+
+include_once '../base_dir.php';
+include_once DIR_ISP . 'MembroDeTimeScrum.php';
+
+class ScrumMaster implements MembroDeTimeScrum
+{
+    public function priorizarBacklog() {  throw new Exception( "Função errada" );  }
+ 
+    public function blindarTime()
+    {
+        return "Blindando Time";
+    }
+ 
+    public function implementarFuncionalidades() {  throw new Exception( "Função errada" );  }
+}
+
+
+?>
